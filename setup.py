644961dc94b93a9ecaf82c5bc2356b2d184032db
Somyajit Chakraborty Sam <somyajitchppr@gmail.com>
2019-12-28 12:57:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
           'jupyter',
           'pillow',
           'nltk',
-          'pyYAML'
+          'pyYAML',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
