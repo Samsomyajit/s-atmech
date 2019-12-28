9c12412ede06ad7f719d664a05661196e03b897e
Somyajit Chakraborty Sam <somyajitchppr@gmail.com>
2019-12-28 12:35:12 +0530

    Update and rename s-atmech.py to AttentionLayer.py

--- AttentionLayer.py
+++ AttentionLayer.py
@@ -120,3 +120,8 @@ class AttentionLayer(Layer):
             tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[1][2])),
             tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[0][1]))
         ]
+
+    """testing"""
+if __name__ == "__main__":
+    lyr = AttentionLayer(lyr)
+    print (lyr.compute_output_shape(10))
